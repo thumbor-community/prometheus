9d877cadd9f1a18d2c4e7b7be63e345e2f1e5d63
vladislav.volchkov <vladislav.volchkov@pandadoc.com>
2024-02-14 11:07:21 +0100

    feat: add multiprocess support

--- tc_prometheus/metrics/prometheus_metrics.py
+++ tc_prometheus/metrics/prometheus_metrics.py
@@ -6,7 +6,8 @@
 # Copyright (c) 2017 Simon Effenberg <savar@schuldeigen.de>
 # Copyright (c) 2017 Thumbor Community Extensions
 
-from prometheus_client import Counter, start_http_server, Summary
+from prometheus_client import Counter, start_http_server, Summary, multiprocess
+from prometheus_client.core import CollectorRegistry
 from thumbor.metrics import BaseMetrics
 
 
@@ -19,7 +20,9 @@ class Metrics(BaseMetrics):
             port = config.PROMETHEUS_SCRAPE_PORT
             if isinstance(port, str):
                 port = int(port)
-            start_http_server(port)
+            registry = CollectorRegistry()
+            multiprocess.MultiProcessCollector(registry)
+            start_http_server(port, registry=registry)
             Metrics.http_server_started = True
             Metrics.counters = {}
             Metrics.summaries = {}
@@ -99,3 +102,4 @@ class Metrics(BaseMetrics):
             if metricname.startswith(mapped + "."):
                 metricname = mapped
         return metricname
+    
